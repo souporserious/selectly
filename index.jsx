0601ecdc2b5398ba9f13b086025b2227e50620a5
Travis Arnold <ftntravis@gmail.com>
2017-02-07 01:21:59 -0800

    publish: replaced parts with React ARIA where applicable 🎉
    generated from commit 6ab7fffa6b1a5d45918c9876b3fc91df99df5c4f

--- index.jsx
+++ index.jsx
@@ -1,7 +1,7 @@
 import React, { Component, Children, PropTypes, cloneElement } from 'react'
 import ReactDOM from 'react-dom'
 import { spring } from 'react-motion'
-import { Select, Option, utils } from '../src/selectly.js'
+import { Select, Trigger, OptionList, Option, utils } from '../src/selectly.js'
 
 import '../src/selectly.scss'
 import './main.scss'
@@ -14,11 +14,7 @@ const { buildOptionsLookup, getCurrentOptions, getToggledOptions, getAllValues, 
 // accessible:
 // http://www.w3.org/TR/WCAG10-HTML-TECHS/#forms
 
-class Trigger extends Component {
-  static contextTypes = {
-    isOpen: PropTypes.bool
-  }
-
+class CustomTrigger extends Component {
   _renderLabel(label) {
     return (
       <span
@@ -31,56 +27,24 @@ class Trigger extends Component {
   }
 
   render() {
-    let { currentValue, emptyValue, isMultiple, isDisabled } = this.props
-    const isActive = this.context.isOpen
-
+    const { currentValue, emptyValue, isMultiple, isDisabled, ...restProps } = this.props
+    const isActive = false
     return (
-      <div
-        tabIndex="1"
-        onKeyDown={this.props.onKeyDown}
-
+      <Trigger
         className={
           'react-select-trigger' +
           (isMultiple ? ' react-select-trigger--multiple' : '') +
           (isActive ? ' react-select-trigger--active' : '') +
           (isDisabled ? ' react-select-trigger--disabled' : '')
         }
+        {...restProps}
       >
         { currentValue.length > 0
           ? currentValue.map(({ label }) => this._renderLabel(label))
           : emptyValue
         }
-      </div>
+      </Trigger>
     )
-  }
-}
-
-function interceptEvent(event) {
-  if (event) {
-    event.preventDefault()
-    event.stopPropagation()
-  }
-}
-
-class SelectTrigger extends Component {
-  render() {
-    return this.props.children
-  }
-}
-
-class SelectOptions extends Component {
-  state = {
-    index: -1
-  }
-
-  componentDidMount() {
-    ReactDOM.findDOMNode(this).focus()
-  }
-
-  render() {
-    return cloneElement(this.props.children, {
-      index: this.state.index
-    })
   }
 }
 
@@ -108,32 +72,40 @@ class MySelect extends Component {
   }
 
   setOpen(isOpen) {
-    this.refs.select.setOpen(isOpen)
+    this._select.setOpen(isOpen)
   }
 
   _renderOption({ value, label }) {
     const hasCheckbox = this.props.checkbox
     const isSelected = isOptionSelected(this.props.value, value)
-
     return (
       <Option
         key={label}
         value={value}
-        className={
-          'react-select-option' +
-          (hasCheckbox ? ' has-checkbox' : '') +
-          (isSelected ? ' is-selected' : '')
-        }
       >
-        { hasCheckbox &&
-          <input
-            type="checkbox"
-            className="react-select-option__checkbox"
-            checked={isSelected}
-            readOnly
-          />
+        {({ props, isHighlighted }) =>
+          <div
+            {...props}
+            className={
+              'react-select-option' +
+              (hasCheckbox ? ' has-checkbox' : '') +
+              (isSelected ? ' is-selected' : '')
+            }
+            style={{
+              background: isHighlighted ? 'orange' : ''
+            }}
+          >
+            { hasCheckbox &&
+              <input
+                type="checkbox"
+                className="react-select-option__checkbox"
+                checked={isSelected}
+                readOnly
+              />
+            }
+            {label}
+          </div>
         }
-        {label}
       </Option>
     )
   }
@@ -191,60 +163,30 @@ class MySelect extends Component {
     )
   }
 
-  _handleKeyDown = (e) => {
-    //interceptEvent(e)
-    if (!this.refs.select.isOpen) {
-      if (e.key === 'ArrowDown') {
-        this.setOpen(true)
-      }
-    } else {
-      if (e.key === 'ArrowDown') {
-        this.moveFocus(1)
-      }
-    }
-  }
-
-  moveFocus(amount) {
-    const len = Object.keys(buildOptionsLookup(this.props.options)).length
-    this.setState({
-      focusedIndex: (this.state.focusedIndex + amount + len) % len
-    })
-  }
-
   render() {
     const { value, emptyValue, options, multiple, onChange, selectAll, deselectAll } = this.props
     const currentOptions = getCurrentOptions(options, value)
 
     return (
       <Select
-        ref="select"
+        ref={c => this._select = c}
         classPrefix="react-select"
         multiple={multiple}
-        onChange={onChange}
       >
-        <SelectTrigger>
-          <Trigger
-            onKeyDown={this._handleKeyDown}
-
-            emptyValue={emptyValue}
-            isMultiple={multiple}
-            currentValue={currentOptions}
-          />
-        </SelectTrigger>
-        <SelectOptions>
-          <div
-            ref="options"
-            tabIndex="2"
-            onKeyDown={this._handleKeyDown}
-
-            className="react-select-menu"
-          >
-            { (selectAll || deselectAll) &&
-              this._renderSelectAll()
-            }
-            {this._renderOptions(options)}
-          </div>
-        </SelectOptions>
+        <CustomTrigger
+          emptyValue={emptyValue}
+          isMultiple={multiple}
+          currentValue={currentOptions}
+        />
+        <OptionList
+          onOptionSelection={onChange}
+          className="react-select-menu"
+        >
+          { (selectAll || deselectAll) &&
+            this._renderSelectAll()
+          }
+          {this._renderOptions(options)}
+        </OptionList>
       </Select>
     )
   }
@@ -273,8 +215,8 @@ class Demo1 extends Component {
     this.refs.select.setOpen(true)
   }
 
-  _handleChange = (value) => {
-    this.setState({currentValue: value})
+  _handleChange = ({ value }) => {
+    this.setState({ currentValue: value })
   }
 
   render() {
@@ -296,35 +238,17 @@ class Demo1 extends Component {
 }
 
 class Demo2 extends Component {
-  // state = {
-  //   currentValue: ['the-shining', 'halloween'],
-  //   options: [
-  //     { value: 'the-shining', label: 'The Shining' },
-  //     { value: 'poltergeist', label: 'Poltergeist' },
-  //     { value: 'halloween', label: 'Halloween' },
-  //     { value: 'pumpkinhead', label: 'Pumpkinhead' }
-  //   ]
-  // }
-
   state = {
-    currentValue: [],
+    currentValue: ['the-shining', 'halloween'],
     options: [
-      { label: 'Dogs', optgroup: [
-        { value: 'beagle', label: 'Beagle' },
-        { value: 'boxer', label: 'Boxer' },
-        { value: 'frenchy', label: 'French Bulldog' },
-        { value: 'pit-bull', label: 'Pit Bull' }
-      ]},
-      { label: 'Cats', optgroup: [
-        { value: 'bengal', label: 'Bengal' },
-        { value: 'egyptian', label: 'Egyptian' },
-        { value: 'munchkin', label: 'Munchkin' },
-        { value: 'persian', label: 'Persian' }
-      ]}
+      { value: 'the-shining', label: 'The Shining' },
+      { value: 'poltergeist', label: 'Poltergeist' },
+      { value: 'halloween', label: 'Halloween' },
+      { value: 'pumpkinhead', label: 'Pumpkinhead' }
     ]
   }
 
-  _handleChange = (value) => {
+  _handleChange = ({ value }) => {
     this.setState({
       currentValue: getToggledOptions(this.state.currentValue, value)
     })
@@ -369,7 +293,7 @@ class MultiSelect extends Component {
     currentValues: []
   }
 
-  _handleChange = (value) => {
+  _handleChange = ({ value }) => {
     this.setState({
       currentValues: getToggledOptions(this.state.currentValues, value)
     })
@@ -378,18 +302,18 @@ class MultiSelect extends Component {
   render() {
     const { defaultValue, currentValues } = this.state
     return (
-      <Select multiple onChange={this._handleChange}>
-        <button>
+      <Select multiple>
+        <Trigger>
           { currentValues.length > 0
             ? currentValues.join(', ')
             : defaultValue
           }
-        </button>
-        <ul>
+        </Trigger>
+        <OptionList onOptionSelection={this._handleChange}>
           <Option value="red">Red</Option>
           <Option value="green">Green</Option>
           <Option value="blue">Blue</Option>
-        </ul>
+        </OptionList>
       </Select>
     )
   }
